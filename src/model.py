d23fe2f55834096bd1d3b2b4ad7052305897f8be
Mark Evers <mevers303@gmail.com>
2018-04-05 14:47:18 -0600

    Made model save right after it trains.  Fixed code in predictions section.

--- src/model.py
+++ src/model.py
@@ -29,10 +29,15 @@ hidden_layer_size = 1024
 # create the model
 model = Sequential()
 model.add(LSTM(units=hidden_layer_size, input_shape=(NUM_STEPS, dataset.n_features)))
-# model.add(LSTM(units=hidden_layer_size, input_shape=(NUM_STEPS, dataset.n_features)))
+# model.add(LSTM(units=hidden_layer_size, input_shape=(NUM_STEPS, dataset.n_features), return_sequences=True))
+# model.add(LSTM(units=hidden_layer_size, return_sequences=True))
+# model.add(LSTM(units=hidden_layer_size))
 model.add(Dense(units=dataset.n_composers, activation='sigmoid'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
 print(model.summary())
+
+
+
 
 # FIT THE MODEL
 # model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
@@ -41,12 +46,23 @@ while dataset.last_train_chunk_i < dataset.n_train_files:
     model.train_on_batch(X, y)
 
 
+print("Saving model to disk")
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+
+
+
+
 # MAKE PREDICTIONS
 predictions = None
 actual = None
 while dataset.last_test_chunk_i < dataset.n_test_files:
     X, y = dataset.get_docs_chunk(CHUNK_SIZE, "test")
-    if actual:
+    if actual != None:
         actual = np.append(actual, y, axis=0)
         predictions = np.append(predictions, model.predict(X), axis=0)
     else:
@@ -64,13 +80,3 @@ for y, y_pred in zip(actual, predictions):
         correct += 1
 
 print("Accuracy:", correct / total)
-
-
-
-print("Saving model to disk")
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
